b09976e11045d200530fc0b77a5d7f55c196ed10
Douglas Paz <eu@douglaspaz.com>
2019-01-04 17:05:23 -0300

    Init get model

---
+++ src/get-model.js
@@ -0,0 +1,19 @@
+import { Model as VueMcModel } from 'vue-mc';
+import getModelDefinition from './get-model-definition';
+
+
+export default (attrsDescription, Model = VueMcModel) => {
+  const modelDefinition = getModelDefinition(attrsDescription);
+  class DRFModel extends Model {
+    // eslint-disable-next-line class-methods-use-this
+    validation() {
+      return modelDefinition.validation;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    mutations() {
+      return modelDefinition.mutations;
+    }
+  }
+  return DRFModel;
+};
