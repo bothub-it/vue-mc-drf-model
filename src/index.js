36a39ed0ed3145749da5ba276d0af5daa6ba984e
Douglas Paz <eu@douglaspaz.com>
2019-01-04 16:16:24 -0300

    Add getModelDefinition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import AttrValidationParse from './attr-validation-parse';
 import AttrMutationParse from './attr-mutation-parse';
+import getModelDefinition from './get-model-definition';
 
 
 export default {
   AttrValidationParse,
   AttrMutationParse,
+  getModelDefinition,
 };
