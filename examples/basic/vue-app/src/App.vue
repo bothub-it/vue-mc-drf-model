99205d4eb329cf0fd7ac80077a0f2ef6ebb326b1
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-18 16:42:29 -0300

    Call component form

--- examples/basic/vue-app/src/App.vue
+++ examples/basic/vue-app/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1>Vue App</h1>
+    <people-form />
   </div>
 </template>
 
 <script>
+import PeopleForm from './sections/PeopleForm';
+
+
 export default {
   name: 'App',
+  components: {
+    PeopleForm,
+  },
 }
 </script>
---
+++ examples/basic/vue-app/src/sections/PeopleForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>People Form</h2>
+  </section>
+</template>
+
+<script>
+import { getPeopleOptions } from '../requests';
+
+
+export default {
+  name: 'PeopleForm',
+  mounted() {
+    getPeopleOptions()
+      .then((response) => {
+        console.log(response);
+      });
+  },
+};
+</script>
