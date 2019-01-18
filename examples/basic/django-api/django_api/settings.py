b4a157694f4a45decbcb2c8ebd96ca755c9dc863
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-18 16:41:02 -0300

    Config django cors middleware, whitelist and installed app

--- examples/basic/django-api/django_api/settings.py
+++ examples/basic/django-api/django_api/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+  'localhost:8080',
+]
 
 # Application definition
 
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_countries',
     'django_api.people',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'django_api.urls'
