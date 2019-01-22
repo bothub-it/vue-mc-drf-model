806a41544d4d6f48ee0bd1b5d6bba6a51a329b12
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-22 10:40:50 -0300

    Add form to vue example

--- examples/basic/vue-app/src/sections/PeopleForm.vue
+++ examples/basic/vue-app/src/sections/PeopleForm.vue
@@ -1,6 +1,21 @@
 <template>
   <section>
     <h2>People Form</h2>
+    <form @submit.prevent="onSubmit()">
+      <input type="text" name="name" placeholder="name">
+      <select name="" id="">
+        <option value="Brasil">Brasil</option>
+        <option value="Albania">Albania</option>
+        <option value="China">China</option>
+      </select>
+      <input type="date" name="bday" placeholder="Birthday">
+      <select name="gender">
+        <option value="M">Male</option>
+        <option value="F">Female</option>
+        <option value="O">Other</option>
+      </select>
+      <input type="submit" value="submit">
+  </form>
   </section>
 </template>
 
@@ -16,5 +31,11 @@ export default {
         console.log(response);
       });
   },
+  methods: {
+    onSubmit() {
+      console.log('ping here');
+      
+    }
+  }
 };
 </script>
