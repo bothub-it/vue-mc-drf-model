942a23c935cfa78ba42bbf482504041f2881d06a
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-29 16:21:19 -0300

    Remove useless import in viewsets

--- examples/basic/django-api/django_api/people/viewsets.py
+++ examples/basic/django-api/django_api/people/viewsets.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 
 from .models import People
 from .serializers import PeopleSerializer
-from .meta import Metadata
 
 
 class PeopleViewset(viewsets.ModelViewSet):
