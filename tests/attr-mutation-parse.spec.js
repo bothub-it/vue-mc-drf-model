6647c1a9c6a810c0668d13c45535d6f0e224d416
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-31 09:37:46 -0300

    Update att-mutation unit test

--- tests/attr-mutation-parse.spec.js
+++ tests/attr-mutation-parse.spec.js
@@ -15,4 +15,64 @@ describe('AttrMutationParse', () => {
       expect(mutation).to.be.equal(String);
     });
   });
+
+  describe('Type intenger', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'integer' });
+    });
+
+    it('is a integer mutation', () => {
+      expect(mutation).to.be.equal(Number);
+    });
+  });
+
+  describe('Type boolean', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'boolean' });
+    });
+
+    it('is a boolean mutation', () => {
+      expect(mutation).to.be.equal(Boolean);
+    });
+  });
+
+  describe('Type email', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'email' });
+    });
+
+    it('is a email mutation', () => {
+      expect(mutation).to.be.equal(String);
+    });
+  });
+
+  describe('Type choice', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'choice' });
+    });
+
+    it('is a choice mutation', () => {
+      expect(mutation).to.be.equal(String);
+    });
+  });
+
+  describe('Type date', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'date' });
+    });
+
+    it('is a date mutation', () => {
+      expect(mutation).to.be.equal(Date);
+    });
+  });
+
+  describe('Type text', () => {
+    beforeEach(() => {
+      mutation = AttrMutationParse({ type: 'text' });
+    });
+
+    it('is a text mutation', () => {
+      expect(mutation).to.be.equal(String);
+    });
+  });
 });
